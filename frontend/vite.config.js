0f78195f334144b019a053b05bfd6d13dcbc25aa
Jose-Ve <joseleonelvr@gmail.com>
2025-12-03 20:15:21 -0600

    Configurando base vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // Usamos la raíz para que los assets se resuelvan bien en Netlify
 export default defineConfig({
-  base: '/',
+  base: '/contabilidad-personal/',
   plugins: [react()],
   server: {
     port: 5173,
